402fa6b2b47b87f4b688d04d1a71d1d84bb5430d
Kieran Smith <kieran.jas.smith@gmail.com>
2023-05-30 21:02:25 +1000

    Experimentation

--- src/test/test_utils/token_utils.rs
+++ src/test/test_utils/token_utils.rs
@@ -1,3 +1,4 @@
+use std::alloc::{alloc, Layout};
 use std::collections::linked_list;
 use std::fmt::{Display, Formatter};
 use std::future::Future;
@@ -42,50 +43,96 @@ pub fn random_token_stream<'a>(len: usize) -> Vec<Token<'a>> {
 
 #[test]
 fn test1() {
-    println!("{}", parse_regexp("[[]"))
+    const size : ((usize, usize, usize, usize,),
+                  (usize, usize, usize, usize,),
+                  (usize, usize, usize, usize,)) = parse_regexp(r"[]");
+
+    let arr = [0u8; size.0.0];
+
+    let ar2 : [T]
+    let val = CharClass::<{ size.0.0 }> {};
+    println!("{}", size.0.0)
+}
+
+enum Pattern {
+    Literal(LiteralPattern)
+}
+
+struct Regexp<const N : usize> {
+
+    subgroups : [Group; N]
 }
 
 struct Group {
 
-    //subgroups : Box<[RegexpGroup; D]>
+}
+
+// keywords and hardcoded values
+struct LiteralPattern<const N : usize> {
+    index: usize,
+    literal: [u8; N]
+}
+
+struct Or2Pattern<A : Pattern, B : Pattern> {
+    a: A,
+    b: B,
+}
+struct Or3Pattern<A : Pattern, B : Pattern, C : Pattern> {
+    a: A,
+    b: B,
+    c: C,
+}
+struct Or4Pattern<A : Pattern, B : Pattern, C : Pattern, D : Pattern> {
+    a: A,
+    b: B,
+    c: C,
+    d: D,
 }
 
 struct CharClass<const N: usize> {
 
 }
 
-trait Empty {
-    type Output;
-    fn empty() -> Output;
-}
-
-macro_rules! gen_fixed_arrays {
-    ($t:ty,$def:literal;$($num:literal)*) => {
-        paste::item! {
-                $(
-                    const fn [<fixed_arr_ $num>]() -> [$t; $num] { [$def; $num] }
-                )*
+macro_rules! arr {
+    ([$lit:literal; $id:ident]) => {
+        match $id {
+            0 =>  [$lit; 0],
+            1 =>  [$lit; 1],
+            2 =>  [$lit; 2],
+            3 =>  [$lit; 3],
+            4 =>  [$lit; 4],
+            5 =>  [$lit; 5],
+            6 =>  [$lit; 6],
+            7 =>  [$lit; 7],
+            8 =>  [$lit; 8],
+            9 =>  [$lit; 9],
+            10 =>  [$lit; 10],
+            _ => panic!("Number outside available range")
         }
     }
 }
 
-gen_fixed_arrays!(u8,0; 0 1 2 3 4 5 6 7 8 9 10);
-
-const fn parse_regexp(string : &str) -> u8 {
-    let mut ret = 0
+/// validates the input and returns a tuple of tuples:
+/// The first array is 4 usizes indicating the lengths of up to 4 character classes
+/// The first usize describes how many character classes there are
+/// The second array is 4 usizes indicating the lengths of up to 4 groups
+/// The second usize describes how many groups there are
+const fn parse_regexp<const N: usize>(string : &str) -> ([usize; N], usize, [usize; N], usize) {
     let bytes = string.as_bytes();
-    let mut i = 0;
+
+    let c_class_sizes = [0usize; N];
+    let group_sizes = [0usize; N];
 
     let mut paren_depth = 0;
     let mut bracket_depth = 0;
     let mut is_escaped = false;
     let mut in_char_class = false;
+    let mut can_be_repeated = false;
 
-    let dyn_arr = match paren_depth {
-        0 => fixed_arr_0(),
-        _ =>
-    }
+    let mut c_class_size_index = 0;
+    let mut group_size_index = 0;
 
+    let mut i = 0;
     while i != bytes.len() {
         let char = bytes[i];
 
@@ -98,28 +145,33 @@ const fn parse_regexp(string : &str) -> u8 {
                     }
                     paren_depth += 1;
                 },
-                b')' => paren_depth -= 1,
+                b')' => {
+                    paren_depth -= 1;
+                    can_be_repeated = true;
+                },
                 b'[' => {
                     in_char_class = true;
                     if bracket_depth == 1 {
                         panic!("Cannot have nested char classes");
                     }
                     bracket_depth += 1;
+                    can_be_repeated = false;
                 },
                 b']' => {
                     in_char_class = false;
                     bracket_depth -= 1;
+                    can_be_repeated = true;
                 },
-                b'*' | b'+' => (),
-
+                b'*' | b'+' => {
+                    if !can_be_repeated {
+                        panic!("Token cannot be repeated ");
+                    }
+                },
+                b'\\' => is_escaped = true,
                 _ => ()
             }
         } else {
-            match char {
-                _ => {
 
-                }
-            }
         }
         i += 1;
     }
@@ -128,5 +180,6 @@ const fn parse_regexp(string : &str) -> u8 {
         panic!("Unbalanced parentheses in regular expression")
     }
 
-    return ret as u8;
+
+
 }
