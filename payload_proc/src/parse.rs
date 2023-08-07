eddac1bf17c70fdac0b47ef6f07466fe84332e7f
kieran.smith <kieran.smith@megaport.com>
2023-08-07 14:33:17 +1000

    Some payload parser progress

--- payload_proc/src/parse.rs
+++ payload_proc/src/parse.rs
@@ -1,6 +1,5 @@
 use crate::expr::{Expr, ExprParser};
 use crate::parse::ParseResult::{Consumed, ParseError};
-use crate::parse::ParseState::{ArgEnd, ArgsEnd, Deferred};
 use crate::root::{Root, RootType};
 
 pub(crate) enum ParseResult {
@@ -21,12 +20,15 @@ enum ParsingRootType {
 // e.g. after parsing '$', would be `PrfixVariable`
 enum ParseState {
     Default,
+    LineCommentStart,
+    LineComment,
     Done,
     Prefix,
     Identifier,
     ArgsEnd,
     ArgEnd,
     AwaitingArgOrArgsEnd,
+    AwaitingDelimOrArgsEnd,
     BodyBegin{ depth: usize },
     BodyEnd{ depth: usize },
     Deferred,
@@ -60,25 +62,47 @@ impl Parser {
         let res = match self.state {
             ParseState::Prefix => self.prefix(char),
             ParseState::Identifier => self.identifier(char),
-            ParseState::ArgStart => {}
-            ParseState::ArgsStart => {}
-            ParseState::Arg => {}
             ParseState::ArgEnd => {}
             ParseState::ArgsEnd => {}
             ParseState::BodyBegin { .. } => {}
             ParseState::BodyEnd { .. } => {}
-            ParseState::Default => {}
-            ParseState::Done => return self.root.take()
+            ParseState::Default => self.default(char),
+            ParseState::Done => return self.root.take(),
+            ParseState::AwaitingArgOrArgsEnd => self.awaiting_arg_or_end(char),
+            ParseState::AwaitingDelimOrArgsEnd => self.awaiting_delim_or_end(char),
+            ParseState::LineCommentStart => self.awaiting_delim_or_end(char),
+            ParseState::Deferred => self.deferred(char),
         };
 
         todo!()
+    }
+
+
+    fn default(&mut self, char: u8) -> ParseResult {
+        match char {
+            b' ' | b'\n' | b'\t' => Consumed,
+            b'/' => {
+                self.state = ParseState::LineCommentStart;
+                Consumed
+            }
+            p @ (b'@' | b'#') => {
+                self.state = ParseState::Prefix;
+                self.root_type = match p {
+                    b'@' => ParsingRootType::Modifier,
+                    b'#' => ParsingRootType::Matcher
+                };
+                Consumed
+            }
+
+            _ => ParseError("Illegal character at start of identifier".to_string())
+        }
     }
 
     fn prefix(&mut self, char: u8) -> ParseResult {
         match char {
             x @ (b'a'..=b'z' | b'A'..=b'Z' | b'_') => {
                 self.accumulator.push(x);
-                ParseResult::Consumed
+                Consumed
             }
             _ => ParseError("Illegal character at start of identifier".to_string())
         }
@@ -88,7 +112,7 @@ impl Parser {
         match char {
             x @ (b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' | b'_') => {
                 self.accumulator.push(x);
-                ParseResult::Consumed
+                Consumed
             }
             _ => ParseError("Illegal character at start of identifier".to_string())
         }
@@ -110,17 +134,47 @@ impl Parser {
         }
     }
 
-    fn arg_end(&mut self, char: u8) -> ParseResult {
+    fn awaiting_delim_or_end(&mut self, char: u8) -> ParseResult {
         match char {
             b' ' => {
-                self.AwaitingArgOrArgsEnd
+                Consumed
+            },
+            b',' => {
+                self.state = ParseState::AwaitingArgOrArgsEnd;
                 Consumed
             },
             b')' => {
-                self.state = ArgsEnd;
+                self.state = ParseState::ArgsEnd;
                 Consumed
             }
             _ => ParseError("Illegal character at start of identifier".to_string())
+        }
+    }
+
+    fn awaiting_arg_or_end(&mut self, char: u8) -> ParseResult {
+        match char {
+            b' ' => {
+                self.state = ParseState::AwaitingDelimOrArgsEnd;
+                Consumed
+            },
+            b')' => {
+                self.state = ParseState::ArgsEnd;
+                Consumed
+            }
+            x => {
+                match self.expr_parser.parse(char) {
+                    Consumed => {
+                        self.state = ParseState::Deferred;
+                        Consumed
+                    },
+                    ParseResult::ParsedExpr(e) => {
+                        self.state = ParseState::AwaitingDelimOrArgsEnd;
+                        Consumed
+                    }
+                    _ => ParseError("Expected start of expression or ')'".to_string())
+
+                }
+            }
         }
     }
 }
